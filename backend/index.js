dc612925c001b0bbcd2ada35ad26a1da38c77dbe
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-25 23:20:02 -0300

    Start frontend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.send("Hello World!!!")
+  return response.send("Hello World!!!");
 });
 
 app.listen(3333);
