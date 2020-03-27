653a4b12184947f5348666b3200ddce102ffdb29
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-27 18:20:50 -0300

    Create Logon component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,13 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
+
+import "./global.css";
+
+import Logon from "./pages/Logon";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-    console.log(counter);
-  }
-
   return (
     <>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
+      <Logon />
     </>
   );
 }
