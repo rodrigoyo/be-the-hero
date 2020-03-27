bd1dcff1dd831c07b224682eeb4f5f2effa8b304
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-27 14:45:27 -0300

    Route create login is up

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const express = require("express");
 const OngController = require("./controller/OngController");
 const IncidentController = require("./controller/IncidentController");
 const ProfileController = require("./controller/ProfileController");
+const SessionController = require("./controller/SessionController");
 
 const routes = express.Router();
+
+routes.post("/sessions",SessionController.create);
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
