1a1808c9c01318576f9c5615376b1f2ddd5fafa4
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-27 10:33:48 -0300

    Include notes on route concepts

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 
+/**
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Recursos são referentes a cada entidade que possa ser chamada da api
+ */
+
+/**
+ * Tipos de parâmetros:
+ *
+ * Query params: Parâmetros nomeados enviados na rota após "?" intercalados por "&". Exemplo: (Filtros, paginação)
+ * Route params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
 app.get("/", (request, response) => {
   return response.send("Hello World!!!");
 });
