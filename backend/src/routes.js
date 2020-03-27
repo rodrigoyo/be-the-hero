b330e403b2b287cad77a00ce93592e43453aadff
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-27 13:33:53 -0300

    Dev resource post ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
+const crypto = require("crypto");
+
 const routes = express.Router();
 
-
-routes.get("/", (request, response) => {
-  return response.send("Hello World!!!");
+routes.post("/ongs", (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+  const id = crypto.randomBytes(4).toString("HEX");
+  console.log(data);
+  return response.json();
 });
 
 module.exports = routes;
