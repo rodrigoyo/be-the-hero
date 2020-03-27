038a114201f0c9d6fe2aa5444ae5f0bf2f9925ad
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-27 14:06:40 -0300

    Implement incident GET/POST in incidents

--- backend/src/controller/OngController.js
+++ backend/src/controller/OngController.js
@@ -6,6 +6,7 @@ module.exports = {
     const ongs = await connection("ongs").select("*");
     return response.json(ongs);
   },
+  
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
     const id = crypto.randomBytes(4).toString("HEX");
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const OngController = require("./controller/OngController")
+
+const OngController = require("./controller/OngController");
+const IncidentController = require("./controller/IncidentController");
 
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
 
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
 module.exports = routes;
