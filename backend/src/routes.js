c30a2f78f148c0a6e4301a6205841b2fa5879cbf
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-27 14:31:16 -0300

    Route Profile is up

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const OngController = require("./controller/OngController");
 const IncidentController = require("./controller/IncidentController");
+const ProfileController = require("./controller/ProfileController");
 
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
+
+routes.get("/profile", ProfileController.index);
 
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
