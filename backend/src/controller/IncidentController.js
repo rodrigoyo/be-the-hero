983c9262b320ec9199af1ca5f6d4395241e193c9
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-27 14:22:01 -0300

    Incident routes implemented

--- backend/src/controller/IncidentController.js
+++ backend/src/controller/IncidentController.js
@@ -17,5 +17,29 @@ module.exports = {
       ong_id
     });
     return response.json({ id });
+  },
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const ong_id = request.headers.authorization;
+
+    const incident = await connection("incidents")
+      .where("id", id)
+      .select("ong_id")
+      .first();
+      
+    if (!incident) {
+      return response.status(404).json({ error: "Incident not finded." });
+    }
+    
+    if (incident.ong_id !== ong_id) {
+      return response.status(401).json({ error: "Operation not permited." });
+    }
+
+    await connection("incidents")
+      .where("id", id)
+      .delete();
+
+    return response.status(204).send();
   }
 };
