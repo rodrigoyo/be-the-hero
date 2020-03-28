963115ce071befb044c0e7305d8d8c4c92b49fb7
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-28 14:58:26 -0300

    Delete incidents is up in page Profile

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -26,6 +26,17 @@ export default function Profile() {
       });
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: { Authorization: ongId }
+      });
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert("Erro ao deletar caso, tente novamente.");
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -58,7 +69,11 @@ export default function Profile() {
             </p>
 
             <button type="button">
-              <FiTrash2 size={20} color="#a8a8b3" />
+              <FiTrash2
+                onClick={() => handleDeleteIncident(incidents.id)}
+                size={20}
+                color="#a8a8b3"
+              />
             </button>
           </li>
         ))}
