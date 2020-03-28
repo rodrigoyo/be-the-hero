35bd36019a7ede480f84af01314fb18cecacc7c6
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-28 11:47:17 -0300

    Include back-link css

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -17,7 +17,7 @@ export default function Logon() {
           <h1>Faça seu logon</h1>
           <input placeholder="Sua ID" />
           <button className="button" type="submit">Entrar</button>
-          <Link to="/register">
+          <Link to="/register" className="back-link">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
           </Link>
