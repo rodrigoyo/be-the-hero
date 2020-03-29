c95579752093be71301076aee94d7a659c156724
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-29 13:21:25 -0300

    Add expo-mail-composer

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -2,18 +2,30 @@ import React from "react";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, Image, TouchableOpacity } from "react-native";
+import * as MailCompose from "expo-mail-composer";
 
 import logoImg from "../../assets/logo.png";
 
 import styles from "./styles";
 
 export default function Detail() {
-
   const navigation = useNavigation();
+  const message =
+    'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00';
 
   function navigateBack() {
     navigation.goBack();
   }
+
+  function sendMail() {
+    MailCompose.composeAsync({
+      subject: "Herói do caso: Cadelinha atropelada",
+      recipients: ["rodrigo.yoshioka@gmail.com"],
+      body: message
+    });
+  }
+
+  function sendWhatsapp() {}
 
   return (
     <View style={styles.container}>
@@ -45,7 +57,7 @@ export default function Detail() {
           <TouchableOpacity style={styles.action} onPress={() => {}}>
             <Text style={styles.actionText}>Whatsapp</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
