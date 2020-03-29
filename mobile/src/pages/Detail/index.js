30b8dc1c7c1e1adcf2749692b29b67c7e64f554e
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-29 13:31:32 -0300

    Implement send whatsapp message

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
 import * as MailCompose from "expo-mail-composer";
 
 import logoImg from "../../assets/logo.png";
@@ -25,7 +25,9 @@ export default function Detail() {
     });
   }
 
-  function sendWhatsapp() {}
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=55011998295300&text=${message}`);
+  }
 
   return (
     <View style={styles.container}>
@@ -54,7 +56,7 @@ export default function Detail() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>Whatsapp</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.action} onPress={sendMail}>
