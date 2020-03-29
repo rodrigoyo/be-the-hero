5be1b07eecaa69bb02ef0989716fb950efc57705
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-29 12:36:06 -0300

    Remove yellobox

--- mobile/App.js
+++ mobile/App.js
@@ -2,9 +2,6 @@ import React from "react";
 
 import Routes from "./src/routes";
 
-import { YellowBox } from "react-native";
-YellowBox.ignoreWarnings(["Remote debugger"]);
-
 export default function App() {
   return <Routes />;
 }
