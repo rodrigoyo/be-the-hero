a6e3430e8c282bb4de0ed79d9a57ffa360915d3f
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-29 11:41:36 -0300

    Add useNavigation in incidents page

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
 
 import logoImg from "../../assets/logo.png";
 import styles from "./styles";
 
 export default function Incidents() {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate("Detail");
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -36,14 +43,16 @@ export default function Incidents() {
             <Text style={styles.incidentProperty}>VALOR:</Text>
             <Text style={styles.incidentValue}>R$ 150,00</Text>
 
-            <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+            >
               <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
               <Feather name="arrow-right" size={16} color="#E02041" />
             </TouchableOpacity>
           </View>
         )}
       />
-
     </View>
   );
 }
