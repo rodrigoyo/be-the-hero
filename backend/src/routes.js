2df698b9d8a5900450de489b6e9a1de23016bb62
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-03-29 15:14:46 -0300

    Add require authorization in profile route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {celebrate, Segments, Joi} = require("celebrate");
+const { celebrate, Segments, Joi } = require("celebrate");
 
 const OngController = require("./controller/OngController");
 const IncidentController = require("./controller/IncidentController");
@@ -8,20 +8,39 @@ const SessionController = require("./controller/SessionController");
 
 const routes = express.Router();
 
-routes.post("/sessions",SessionController.create);
+routes.post("/sessions", SessionController.create);
 
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(10).max(11),
-    city: Joi.string().required(),
-    uf: Joi.string().required().length(2)
-  })
-}), OngController.create);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
 
-routes.get("/profile", ProfileController.index);
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
 
 routes.get("/incidents", IncidentController.index);
 routes.post("/incidents", IncidentController.create);
